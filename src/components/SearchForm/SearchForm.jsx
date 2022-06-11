eacb353fb3b1244f628fa27fe28cf51b0ac6dfa6
Denys-Saveliev <90144527+Denys-Saveliev@users.noreply.github.com>
2022-06-11 23:35:42 +0300

    Update SearchForm.jsx

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import s from './SearchForm.module.css';
 import { toast } from 'react-toastify';
-import PropTypes from 'prop-types';
 import IconButton from 'components/IconButton';
 import { ReactComponent as SearchIcon } from '../../Icons/search.svg';
 
