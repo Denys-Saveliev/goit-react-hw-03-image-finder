046a4810fe4f92620c3fa67ee58aa0dbc0eee0a6
Denys-Saveliev <90144527+Denys-Saveliev@users.noreply.github.com>
2022-06-12 12:09:06 +0300

    Update ImageGallery.jsx

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,23 +1,54 @@
-import React from 'react';
+import { Component } from 'react';
 import s from './ImageGallery.module.css';
 import PropTypes from 'prop-types';
 import ImageGalleryItem from 'components/ImageGalleryItem';
+import { Events, animateScroll as scroll } from 'react-scroll';
 
-const ImageGallery = ({ imageGalleryItems, onGalleryItemClick }) => {
-  return (
-    <ul className={s.ImageGallery}>
-      {imageGalleryItems.map(({ id, webformatURL, tags }) => (
-        <ImageGalleryItem
-          key={id}
-          webformatURL={webformatURL}
-          tags={tags}
-          id={id}
-          onItemClick={() => onGalleryItemClick(id)}
-        />
-      ))}
-    </ul>
-  );
-};
+class ImageGallery extends Component {
+  componentDidMount() {
+    Events.scrollEvent.register('begin', function () {
+      //console.log('begin', arguments);
+    });
+
+    Events.scrollEvent.register('end', function () {
+      //console.log('end', arguments);
+    });
+  }
+
+  componentDidUpdate(prevProps) {
+    if (
+      prevProps.imageGalleryItems.length !== this.props.imageGalleryItems.length
+    ) {
+      scroll.scrollToBottom();
+    }
+  }
+
+  componentWillUnmount() {
+    Events.scrollEvent.remove('begin');
+    Events.scrollEvent.remove('end');
+  }
+
+  onGalleryItemClick = id => {
+    this.props.onGalleryItemClick(id);
+  };
+
+  render() {
+    const { imageGalleryItems } = this.props;
+    return (
+      <ul className={s.ImageGallery}>
+        {imageGalleryItems.map(({ id, webformatURL, tags }) => (
+          <ImageGalleryItem
+            key={id}
+            webformatURL={webformatURL}
+            tags={tags}
+            id={id}
+            onItemClick={() => this.onGalleryItemClick(id)}
+          />
+        ))}
+      </ul>
+    );
+  }
+}
 
 export default ImageGallery;
 
